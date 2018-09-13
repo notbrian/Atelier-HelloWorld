71eeca9dfc27c843b852988236db96c339c68619
Brian Nguyen <nguyen.brian70@gmail.com>
2018-09-13 11:41:06 -0400

    Add Sketch 2 tweaks

--- Sketch 2/script.js
+++ Sketch 2/script.js
@@ -2,22 +2,29 @@ var mouseClicks = [];
 
 function setup() {
     createCanvas(720, 480)
+    var button = createButton('Add Particle')
+    button.mousePressed(addParticle);
 }
 
 function draw() {
+    translate(width/2, height/2)
     for (let i = 0; i < mouseClicks.length; i++) {
+        fill(mouseClicks[i].color);
+        noStroke();
         ellipse(mouseClicks[i].x,mouseClicks[i].y, 10)
-        mouseClicks[i].x = mouseClicks[i].x + mouseClicks[i].speed
-        mouseClicks[i].y = mouseClicks[i].y + mouseClicks[i].speed
+        mouseClicks[i].x = mouseClicks[i].x + mouseClicks[i].xSpeed
+        mouseClicks[i].y = mouseClicks[i].y + mouseClicks[i].ySpeed
 
     }
 }
 
-function mouseClicked() {
+function addParticle() {
     var clickPosition = {
-        x: mouseX,
-        y: mouseY,
-        speed: random(-2, 2)
+        x: 0,
+        y: 0,
+        xSpeed: random(-2, 2),
+        ySpeed: random(-2, 2),
+        color: color(random(100, 255), random(100, 255), random(100, 255))
     }
    
     mouseClicks.push(clickPosition)
