3a9dc95575213436247ee4c02f5b111e3e948fa1
Brian Nguyen <nguyen.brian70@gmail.com>
2018-09-13 12:39:17 -0400

    Add changes

--- Sketch 3/script.js
+++ Sketch 3/script.js
@@ -1,18 +1,35 @@
 
 var mySound
-
+var bubble
 function setup() {
     createCanvas(720, 480)
     mySound = createAudio('sound.wav')
+    bubble = {
+        x: width/2,
+        y: height/2,
+        r: 15
+    }
 }
 
 function draw() {
     background(255)
-    ellipse(width/2, height/2, 30)
+    ellipse(bubble.x, bubble.y, bubble.r * 2)
+    bubble.x = bubble.x + random(-5, 5);
+    bubble.y = bubble.y + random(-5, 5);
+
+    if(bubble.x > width | bubble.x < 0) {
+        bubble.x = width/2
+        bubble.y = height/2
+    }
+    if(bubble.y > height | bubble.y < 0) {
+        bubble.x = width/2
+        bubble.y = height/2
+    }
+
 }
 
 function mouseClicked() {
-    if(dist(mouseX, mouseY, width/2, height/2) <= 15) {
+    if(dist(mouseX, mouseY, bubble.x, bubble.y) <= bubble.r) {
         fill(100)
         mySound.play();
     }
